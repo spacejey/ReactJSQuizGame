88fa5186e5184f7034045af8d167839fc95fc3a2
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-06-07 21:23:21 +0100

    edit dummy data

--- src/components/common/DummyData.js
+++ src/components/common/DummyData.js
@@ -198,75 +198,421 @@ const DummyData = [
       { answerText: 'Henri Matisse', isCorrect: false }
     ],
   },
-
   {
     category: 'Fashion',
-    id: 4,
+    id: 2,
+    question_id: 16,
     difficulty: 'Easy',
-    question: 'Who is the founder of the fashion brand \'Dior\'?',
+    question: 'Which fashion designer is known for creating the little black dress?',
     answerOptions: [
-      { answerText: 'Ralph Lauren', isCorrect: false },
-      { answerText: 'Giorgio Armani', isCorrect: false },
-      { answerText: 'Christian Dior', isCorrect: true }
-    ],
-  },
-  {
-    category: 'Fashion',
-    id: 5,
-    difficulty: 'Medium',
-    question: 'Which designer is known for the creation of the \'Little Black Dress\'?',
-    answerOptions: [
-      { answerText: 'Alexander McQueen', isCorrect: false },
       { answerText: 'Coco Chanel', isCorrect: true },
-      { answerText: 'Vivienne Westwood', isCorrect: false }
+      { answerText: 'Giorgio Armani', isCorrect: false },
+      { answerText: 'Alexander McQueen', isCorrect: false },
+      { answerText: 'Christian Dior', isCorrect: false }
     ],
   },
+    
   {
     category: 'Fashion',
-    id: 6,
-    difficulty: 'Hard',
-    question: 'Which fashion house is famous for its red-soled shoes?',
+    id: 2,
+    question_id: 17,
+    difficulty: 'Easy',
+    question: 'What is the name of the iconic French luxury brand known for its monogrammed handbags?',
     answerOptions: [
+      { answerText: 'Louis Vuitton', isCorrect: true },
       { answerText: 'Gucci', isCorrect: false },
       { answerText: 'Prada', isCorrect: false },
-      { answerText: 'Christian Louboutin', isCorrect: true }
+      { answerText: 'Chanel', isCorrect: false }
     ],
   },
-
+    
   {
-    category: 'Music',
-    id: 7,
+    category: 'Fashion',
+    id: 2,
+    question_id: 18,
     difficulty: 'Easy',
-    question: 'Which artist released the song \'Shape of You\'?',
+    question: 'Which fashion capital is famous for its biannual fashion weeks?',
     answerOptions: [
-      { answerText: 'Justin Bieber', isCorrect: false },
-      { answerText: 'Ed Sheeran', isCorrect: true },
-      { answerText: 'Taylor Swift', isCorrect: false }
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'Milan', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'New York', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 19,
+    difficulty: 'Easy',
+    question: 'Which fabric is commonly used to make denim jeans?',
+    answerOptions: [
+      { answerText: 'Cotton', isCorrect: true },
+      { answerText: 'Silk', isCorrect: false },
+      { answerText: 'Polyester', isCorrect: false },
+      { answerText: 'Wool', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 20,
+    difficulty: 'Easy',
+    question: 'What is the name of the famous red-soled shoe brand?',
+    answerOptions: [
+      { answerText: 'Christian Louboutin', isCorrect: true },
+      { answerText: 'Jimmy Choo', isCorrect: false },
+      { answerText: 'Manolo Blahnik', isCorrect: false },
+      { answerText: 'Prada', isCorrect: false }
     ],
   },
   {
-    category: 'Music',
-    id: 8,
+    category: 'Fashion',
+    id: 2,
+    question_id: 21,
     difficulty: 'Medium',
-    question: 'Who is the lead vocalist of the band \'Coldplay\'?',
+    question: 'Which fashion designer is known for the creation of the wrap dress?',
     answerOptions: [
-      { answerText: 'Chris Martin', isCorrect: true },
-      { answerText: 'Bono', isCorrect: false },
+      { answerText: 'Diane von Furstenberg', isCorrect: true },
+      { answerText: 'Donatella Versace', isCorrect: false },
+      { answerText: 'Ralph Lauren', isCorrect: false },
+      { answerText: 'Tom Ford', isCorrect: false }
+    ],
+  },
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 22,
+    difficulty: 'Medium',
+    question: 'What is the name of the famous French fashion house known for its quilted handbags?',
+    answerOptions: [
+      { answerText: 'Chanel', isCorrect: true },
+      { answerText: 'Hermès', isCorrect: false },
+      { answerText: 'Givenchy', isCorrect: false },
+      { answerText: 'Yves Saint Laurent', isCorrect: false }
+    ],
+  },
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 23,
+    difficulty: 'Medium',
+    question: 'Which fashion model was nicknamed "The Body" in the 1980s?',
+    answerOptions: [
+      { answerText: 'Elle Macpherson', isCorrect: true },
+      { answerText: 'Cindy Crawford', isCorrect: false },
+      { answerText: 'Naomi Campbell', isCorrect: false },
+      { answerText: 'Kate Moss', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 24,
+    difficulty: 'Medium',
+    question: 'Which luxury brand is known for its iconic trench coats?',
+    answerOptions: [
+      { answerText: 'Burberry', isCorrect: true },
+      { answerText: 'Dolce & Gabbana', isCorrect: false },
+      { answerText: 'Versace', isCorrect: false },
+      { answerText: 'Gucci', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 25,
+    difficulty: 'Medium',
+    question: 'Who is the creative director of the fashion brand Gucci as of 2021?',
+    answerOptions: [
+      { answerText: 'Alessandro Michele', isCorrect: true },
+      { answerText: 'Karl Lagerfeld', isCorrect: false },
+      { answerText: 'Stella McCartney', isCorrect: false },
+      { answerText: 'Miuccia Prada', isCorrect: false }
+    ],
+  },
+    
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 26,
+    difficulty: 'Hard',
+    question: 'Which fashion designer is famous for introducing the concept of the "New Look"?',
+    answerOptions: [
+      { answerText: 'Christian Dior', isCorrect: true },
+      { answerText: 'Alexander McQueen', isCorrect: false },
+      { answerText: 'Yves Saint Laurent', isCorrect: false },
+      { answerText: 'Coco Chanel', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 27,
+    difficulty: 'Hard',
+    question: 'Which supermodel popularized the phrase "We don\'t wake up for less than $10,000 a day"?',
+    answerOptions: [
+      { answerText: 'Linda Evangelista', isCorrect: true },
+      { answerText: 'Tyra Banks', isCorrect: false },
+      { answerText: 'Christy Turlington', isCorrect: false },
+      { answerText: 'Naomi Campbell', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 28,
+    difficulty: 'Hard',
+    question: 'Which Italian fashion designer founded the brand Valentino?',
+    answerOptions: [
+      { answerText: 'Valentino Garavani', isCorrect: true },
+      { answerText: 'Roberto Cavalli', isCorrect: false },
+      { answerText: 'Giorgio Armani', isCorrect: false },
+      { answerText: 'Gianfranco Ferré', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 29,
+    difficulty: 'Hard',
+    question: 'Which fashion designer is known for creating the "Trinity" ring?',
+    answerOptions: [
+      { answerText: 'Cartier', isCorrect: true },
+      { answerText: 'Tiffany & Co.', isCorrect: false },
+      { answerText: 'Chopard', isCorrect: false },
+      { answerText: 'Van Cleef & Arpels', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Fashion',
+    id: 2,
+    question_id: 30,
+    difficulty: 'Hard',
+    question: 'Which fashion designer famously said, "In order to be irreplaceable, one must always be different"?',
+    answerOptions: [
+      { answerText: 'Coco Chanel', isCorrect: true },
+      { answerText: 'Karl Lagerfeld', isCorrect: false },
+      { answerText: 'Yves Saint Laurent', isCorrect: false },
+      { answerText: 'Donatella Versace', isCorrect: false }
+    ],
+  },
+    
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 1,
+    difficulty: 'Easy',
+    question: 'Which British band is known for the hit song "Yellow"?',
+    answerOptions: [
+      { answerText: 'Coldplay', isCorrect: true },
+      { answerText: 'The Beatles', isCorrect: false },
+      { answerText: 'One Direction', isCorrect: false },
+      { answerText: 'Queen', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 2,
+    difficulty: 'Easy',
+    question: 'Who is the lead singer of the band U2?',
+    answerOptions: [
+      { answerText: 'Bono', isCorrect: true },
+      { answerText: 'Chris Martin', isCorrect: false },
+      { answerText: 'Mick Jagger', isCorrect: false },
       { answerText: 'Freddie Mercury', isCorrect: false }
     ],
   },
+    
   {
     category: 'Music',
-    id: 9,
-    difficulty: 'Hard',
-    question: 'Which singer\'s real name is Robyn Fenty?',
+    id: 3,
+    question_id: 3,
+    difficulty: 'Easy',
+    question: 'Which pop star is known as the "King of Pop"?',
     answerOptions: [
-      { answerText: 'Rihanna', isCorrect: false },
+      { answerText: 'Michael Jackson', isCorrect: true },
+      { answerText: 'Elvis Presley', isCorrect: false },
+      { answerText: 'Justin Bieber', isCorrect: false },
+      { answerText: 'Prince', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 4,
+    difficulty: 'Easy',
+    question: 'What is the best-selling album of all time?',
+    answerOptions: [
+      { answerText: 'Thriller by Michael Jackson', isCorrect: true },
+      { answerText: 'Back in Black by AC/DC', isCorrect: false },
+      { answerText: 'The Dark Side of the Moon by Pink Floyd', isCorrect: false },
+      { answerText: 'Sgt. Pepper\'s Lonely Hearts Club Band by The Beatles', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 5,
+    difficulty: 'Easy',
+    question: 'Who is the Canadian singer-songwriter known for the hit song "Sorry"?',
+    answerOptions: [
+      { answerText: 'Justin Bieber', isCorrect: true },
+      { answerText: 'Drake', isCorrect: false },
+      { answerText: 'The Weeknd', isCorrect: false },
+      { answerText: 'Shawn Mendes', isCorrect: false }
+    ],
+  },
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 2,
+    difficulty: 'Medium',
+    question: 'Which artist released the album "1989"?',
+    answerOptions: [
+      { answerText: 'Taylor Swift', isCorrect: true },
       { answerText: 'Beyoncé', isCorrect: false },
-      { answerText: 'Rihanna', isCorrect: true }
+      { answerText: 'Adele', isCorrect: false },
+      { answerText: 'Rihanna', isCorrect: false }
+    ],
+  },
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 3,
+    difficulty: 'Medium',
+    question: 'Which band is known for the song "Hotel California"?',
+    answerOptions: [
+      { answerText: 'Eagles', isCorrect: true },
+      { answerText: 'Rolling Stones', isCorrect: false },
+      { answerText: 'Led Zeppelin', isCorrect: false },
+      { answerText: 'Guns N\' Roses', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 4,
+    difficulty: 'Medium',
+    question: 'Who is the lead vocalist of the band Radiohead?',
+    answerOptions: [
+      { answerText: 'Thom Yorke', isCorrect: true },
+      { answerText: 'Chris Martin', isCorrect: false },
+      { answerText: 'Eddie Vedder', isCorrect: false },
+      { answerText: 'Dave Grohl', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 5,
+    difficulty: 'Medium',
+    question: 'Which American singer is known as the "Queen of Soul"?',
+    answerOptions: [
+      { answerText: 'Aretha Franklin', isCorrect: true },
+      { answerText: 'Whitney Houston', isCorrect: false },
+      { answerText: 'Tina Turner', isCorrect: false },
+      { answerText: 'Etta James', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 6,
+    difficulty: 'Medium',
+    question: 'Which band is known for the song "Bohemian Rhapsody"?',
+    answerOptions: [
+      { answerText: 'Queen', isCorrect: true },
+      { answerText: 'The Beatles', isCorrect: false },
+      { answerText: 'Nirvana', isCorrect: false },
+      { answerText: 'AC/DC', isCorrect: false }
+    ],
+  },
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 7,
+    difficulty: 'Hard',
+    question: 'Who is the lead guitarist of the band Guns N\' Roses?',
+    answerOptions: [
+      { answerText: 'Slash', isCorrect: true },
+      { answerText: 'Jimi Hendrix', isCorrect: false },
+      { answerText: 'Eddie Van Halen', isCorrect: false },
+      { answerText: 'Eric Clapton', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 8,
+    difficulty: 'Hard',
+    question: 'Which artist released the album "Thriller"?',
+    answerOptions: [
+      { answerText: 'Michael Jackson', isCorrect: true },
+      { answerText: 'Elvis Presley', isCorrect: false },
+      { answerText: 'Madonna', isCorrect: false },
+      { answerText: 'David Bowie', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 9,
+    difficulty: 'Hard',
+    question: 'Which band released the album "Dark Side of the Moon"?',
+    answerOptions: [
+      { answerText: 'Pink Floyd', isCorrect: true },
+      { answerText: 'The Rolling Stones', isCorrect: false },
+      { answerText: 'Led Zeppelin', isCorrect: false },
+      { answerText: 'The Who', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 10,
+    difficulty: 'Hard',
+    question: 'Who is the lead singer of the band U2?',
+    answerOptions: [
+      { answerText: 'Bono', isCorrect: true },
+      { answerText: 'Chris Martin', isCorrect: false },
+      { answerText: 'Brandon Flowers', isCorrect: false },
+      { answerText: 'Eddie Vedder', isCorrect: false }
+    ],
+  },
+    
+  {
+    category: 'Music',
+    id: 3,
+    question_id: 11,
+    difficulty: 'Hard',
+    question: 'Which artist released the song "Like a Rolling Stone"?',
+    answerOptions: [
+      { answerText: 'Bob Dylan', isCorrect: true },
+      { answerText: 'Bruce Springsteen', isCorrect: false },
+      { answerText: 'Neil Young', isCorrect: false },
+      { answerText: 'Tom Petty', isCorrect: false }
     ],
   }
-
 ]
 
 export default DummyData
