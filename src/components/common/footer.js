761d71794b4df2d4e824d3258e8bc72a3f933807
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-06-16 21:43:10 +0100

    doneeee

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <>
-      <div className='footer'>
-        <span className='footer-text'>This app is made by EunyeongJeong.</span>
+    <div className="animated-title">
+      <div className="track">
+        <div className="content">
+          &nbsp; &#128152; This application is made with React, SASS by Eunyeong
+          &nbsp; &#128126; Do you like Quiz?
+          &nbsp; &#128175; If you&#39;re up for it, bring it on!
+          &nbsp; &#128679; If you&#39;ve found a bug, please let me know
+          &nbsp; &#128233; Email: eunyoung.jeong00@gmail.com
+          &nbsp; &#128153; LinkedIn: eunyeong-jeong/
+          &nbsp; &#128152; This application is made with React, SASS by Eunyeong
+          &nbsp; &#128126; Do you like Quiz?
+          &nbsp; &#128175; If you&#39;re up for it, bring it on!
+          &nbsp; &#128679; If you&#39;ve found a bug, please let me know
+          &nbsp; &#128233; Email: eunyoung.jeong00@gmail.com
+          &nbsp; &#128153; LinkedIn: eunyeong-jeong/</div>
       </div>
-    </>
+    </div>
   )
 }
 
